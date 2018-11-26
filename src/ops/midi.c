c53670057a99cb7d6854d58f9c838b66bed99ed7
james-everitt <38512199+james-everitt@users.noreply.github.com>
2018-11-26 12:05:45 +1100

    Update midi.c

--- src/ops/midi.c
+++ src/ops/midi.c
@@ -11,13 +11,15 @@ static void op_MIDI_CC_set(const void *data, scene_state_t *ss, exec_state_t *es
     command_state_t *cs);
 
 
-const tele_op_t op_MIDI_CC = MAKE_GET_OP(MIDI.CC, op_MIDI_CC_set, 1, true);
+const tele_op_t op_MIDI_CC = MAKE_GET_OP(MIDI.CC, op_MIDI_CC_set, 3, false);
 
 
 static void op_MIDI_CC_set(const void *NOTUSED(data),
                            scene_state_t *NOTUSED(ss), exec_state_t *NOTUSED(es), command_state_t *cs)
 {
-    int16_t input = cs_pop(cs);
-    uint8_t d[] = { MIDI_CC, input };
-    tele_ii_tx(MIDI_ADDR, d, 2);
+    s16 ch = cs_pop(cs);
+    s16 cc = cs_pop(cs);
+    s16 val = cs_pop(cs);
+    uint8_t d[] = { MIDI_CC, ch, cc, val };
+    tele_ii_tx(MIDI_ADDR, d, 4);
 }
